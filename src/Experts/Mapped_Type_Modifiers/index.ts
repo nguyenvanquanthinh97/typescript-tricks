409ede1fe9f90fe7eb89734b4d7ce8a977212f19
thinhvqnguyen <thinh.nvq.js220597@gmail.com>
2022-01-18 10:20:32 +0700

    Extra

--- src/Experts/Mapped_Type_Modifiers/index.ts
+++ src/Experts/Mapped_Type_Modifiers/index.ts
@@ -23,5 +23,39 @@ class State<T extends Object> {
 }
 
 const state = new State({x: 0, y: 0})
-state.update({y: 103})
-console.log(state.current)
+state.update({y: 103}) // Partial: no need to provide `x`.
+console.log(state.current) // Update Successful { x: 0, y: 103}
+
+
+// --------------------------------
+type Required<T> = {
+  [key in keyof T]-?: T[key]
+}
+
+type PartialPoint = {x?: number, y?: number}
+type RequiredPoint = Required<PartialPoint> // {x: number, y: number}
+
+type CircleConfig = {
+  color?: string,
+  radius?: number
+}
+
+class Circle {
+  // Required: Internally all members will always be present
+  private config: Required<CircleConfig>
+
+  constructor(config: CircleConfig){
+    this.config = {
+      color: config.color ?? 'green',
+      radius: config.radius ?? 10
+    }
+  }
+
+  draw() {
+    console.log(
+      'Drawing a circle.',
+      'Color', this.config.color,
+      'Radius', this.config.radius
+    )
+  }
+}
--- src/Experts/Mapped_Types/index.ts
+++ src/Experts/Mapped_Types/index.ts
@@ -1,15 +1,25 @@
 type Point = {
-    x: number,
-    y: number,
-    z: number
-}
+  x: number;
+  y: number;
+  z: number;
+};
 
 type ReadOnly<T> = {
-    readonly [Item in keyof T]: T[Item]
-}
+  readonly [Item in keyof T]: T[Item];
+};
 
 const center: ReadOnly<Point> = {
-    x: 0,
-    y: 0,
-    z: 0
+  x: 0,
+  y: 0,
+  z: 0,
+};
+
+function makeReadOnly<T>(object: T): ReadOnly<T> {
+  return Object.freeze({ ...object });
 }
+
+const editablePoint = { x: 0, y: 0 };
+editablePoint.x = 2;
+
+const readonlyPoint = makeReadOnly({ x: 1, y: 1 });
+readonlyPoint.x = 3; // Error
